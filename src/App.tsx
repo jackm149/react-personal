94727b28aaf4dcf86b305448ce2e9db4e8661765
JACK MALLETT <JACKMUD@UDEL.EDU>
2026-03-24 01:51:37 -0400

    exec calls to gsheet via react wow

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,29 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 export default function App(): React.JSX.Element {
+    //sending to googleSheet
+    function sendToSheet() {
+        fetch(
+            "https://script.google.com/macros/s/AKfycbwK7HN6e5rKJBMa2TiTSLpv4JY9KF15alWJd0om1LnfSZEVXMfhW3Sz3ngOXXhGH9kr/exec",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    message: "chief keef",
+                }),
+                headers: {
+                    "Content-Type": "text/plain",
+                },
+            },
+        );
+    }
+
     return (
         <div className="app">
             <div className="clock">
                 <Clock />
             </div>
             <div className="buttonDefault">
-                <Button>Send to GSheet</Button>
+                <Button onClick={sendToSheet}>Send to GSheet</Button>
             </div>
         </div>
     );
